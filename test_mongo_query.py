3abf6e617bf455ea5fb54a7a9326281e5dc6508a
ShaneDonavanJr <111189533+ShaneDonavanJr@users.noreply.github.com>
2025-03-14 16:36:14 -0400

    Added package detail and fixes to work like

--- test_mongo_query.py
+++ test_mongo_query.py
@@ -1,83 +1,61 @@
-from pymongo import MongoClient
-import pandas as pd
 import json
+import datetime
+from pymongo import MongoClient
 
-MONGO_URL = "mongodb+srv://shane-compass:Gi98VaSEbG3KG3lW@circle-alldata.ok8naze.mongodb.net/?retryWrites=true&w=majority"
-
-# Connect to MongoDB
-client = MongoClient(MONGO_URL)
-db = client["CarrierSalesQuotes"]
-collection = db["LaneInfo"]
-
-# Example coordinates
-pickup_point = [-90.373859, 38.4441963]  # [longitude, latitude]
-delivery_point = [-83.9737935, 35.7895271]  # [longitude, latitude]
-distance_miles = 50
-
-# Convert miles to radians for $geoWithin
-earth_radius_miles = 3963.2
-distance_radians = distance_miles / earth_radius_miles
-
-# Use $geoWithin for both locations (doesn't use geoNear)
-results = collection.find({
-    "pickup.location": {
-        "$geoWithin": {
-            "$centerSphere": [pickup_point, distance_radians]
-        }
-    },
-    "delivery.location": {
-        "$geoWithin": {
-            "$centerSphere": [delivery_point, distance_radians]
-        }
-    },
-    # "active": True,
-    # "carriers.active": True
-},
-{
-  "_id": 0,
-  "customerId": 1,
-  "truckType": 1,
-  "comments": 1, 
-  "carriers.carrier_name": 1,
-  "carriers.carrier_mc": 1,
-  "carriers.contact_name": 1,
-  "carriers.contact_email": 1,
-  "carriers.contact_phone": 1
-}
-)
-
-# Method 2: Convert cursor to list (careful with large result sets)
-results_list = list(results)
-print(results_list)
-
-
-def transform_data(input_data):
-    transformed_data = {"data": []}
+def lambda_handler(event, context):
+    # MongoDB connection
+    myclient = MongoClient("mongodb+srv://vince-circleconnect-dev:7rtd6EBamOJgXoUd@circle-alldata.ok8naze.mongodb.net/LoadDetail?retryWrites=true&w=majority")
+    mydb = myclient["LoadDetail"]
+    mycol = mydb["extensionsummaries"]
     
-    for entry in input_data:
-        carriers = entry['carriers']
-        dedicated = True if entry['customerId'] == '6451' else False
-        for carrier in carriers:
-            carrier_info = {
-                "carrierName": carrier['carrier_name'],
-                "dotNumber": "",  # Assuming dot number is not provided, leave it blank or adjust accordingly
-                "mcNumber": carrier['carrier_mc'],
-                "numberOfTrucks": 0,  # Assuming number of trucks is not provided, leave as 0
-                "equipmentType": entry['truckType'],
-                "dedicated": dedicated,  # Assuming "dedicated" is false unless stated otherwise
-                "comments": entry['comments'],
-                "contacts": [{
-                    "name": carrier['contact_name'],
-                    "phone": carrier['contact_phone'].strip(),
-                    "email": carrier['contact_email'].strip()
-                }]
-            }
-            transformed_data["data"].append(carrier_info)
+    # Date range
+    today = datetime.datetime.today().replace(microsecond=0)
+    yesterday = today - datetime.timedelta(days=465)
     
-    return transformed_data
+    # Romoved 8108 and 1437
+    
+    # Extensions array
+    extensions = [
+      "1492", "8754", "8489", "1536", "8006", "8031", "1341", "8132", "1205",
+      "8109", "8130", "1378", "8030", "1386", "8230", "1226", "8652", "8112",
+      "8231", "8424", "8421", "8012", "8011", "8156", "8017", "8456", "8089", 
+      "8200","8218", "8165", "8027", "1537", "1187", "1038", "1666", "8145", 
+      "8206", "1452","8090", "8740", "8246", "8426", "1600","1603","1897",
+      "1422","1846","1425","1424","1195","1854", "8078", "8076", "8320"
+    ]
+    
+    #  Glen's Team
+    # "1600","1603","1897","1422","1846","1425","1424","1195","1854"
 
-formatted_data = transform_data(results_list)
-print(formatted_data)
 
-pd.DataFrame(formatted_data).to_json("output.json")
 
+
+
+
+
+
+    # MongoDB query
+    query = { 
+        "extension": { "$in": extensions},
+        "date": {"$gte": yesterday}
+    }
+    projection = {
+        "_id": 0, 
+        "extension": 1,
+        "date": 1,
+        "externalInboundAnswered": 1, 
+        "externalOutboundAnswered": 1, 
+        "totalTalkTime": 1
+    }
+    
+    # Execute query
+    results = list(mycol.find(query, projection))
+    
+    # # Close MongoDB connection
+    myclient.close()
+    
+    # Return results
+    return {
+        'statusCode': 200,
+        'body': json.dumps(results, default=str)
+    }
